06fae81197c3c699fa823773add1f3e774fc032f
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-06-29 23:09:19 +0300

    khjh

--- src/api.py
+++ src/api.py
@@ -1,7 +1,6 @@
 import pynvml
-import pyamdgpuinfo
 import pyautogui
-
+import amdsmi
 
 def is_int(n):
     try:
@@ -16,7 +15,6 @@ def detect_platform(n):
         return "Nvidia"
     except Exception:
         try:
-            import amdsmi
             pyamdgpuinfo.get_gpu(n)
             return "AMD"
         except Exception:
@@ -78,7 +76,10 @@ def get_temp():
             pynvml.NVML_TEMPERATURE_GPU
         )
     else:
-        return 0
+        # AMD
+        devices = amdsmi_get_processor_handles()
+        temp = amdsmi.amdsmi_get_temp_metric(devices[0], amdsmi.AmdSmiTemperatureType.EDGE, amdsmi.AmdSmiTemperatureMetric.CURRENT)/1000
+        return round(temp, 1)
 
 def get_vram():
     PLATRFORM = detect_platform(0)
@@ -86,9 +87,12 @@ def get_vram():
         return 0;
     elif PLATRFORM == "Nvidia":
         info = pynvml.nvmlDeviceGetMemoryInfo(pynvml.nvmlDeviceGetHandleByIndex(0))
-        return info.total/1000000
+        return round(info.total/1000000, 2)
     else:
-        return 0
+        # AMD
+        devices = amdsmi_get_processor_handles()
+        info = amdsmi.amdsmi_get_gpu_vram_usage(devices[0])
+        return round(info.get("vram_total"), 2)
 
 def get_vram_free():
     PLATRFORM = detect_platform(0)
@@ -96,9 +100,12 @@ def get_vram_free():
         return 0;
     elif PLATRFORM == "Nvidia":
         info = pynvml.nvmlDeviceGetMemoryInfo(pynvml.nvmlDeviceGetHandleByIndex(0))
-        return info.free/1000000
+        return round(info.free/1000000, 2)
     else:
-        return 0
+        # AMD
+        devices = amdsmi_get_processor_handles()
+        info = amdsmi.amdsmi_get_gpu_vram_usage(devices[0])
+        return round(info.get("vram_total")-info.get("vram_used"), 2)
 
 
 def get_gpu_name():
@@ -110,3 +117,38 @@ def get_gpu_name():
     else:
         amdsmi.amdsmi_init()
         return amdsmi.amdsmi_get_gpu_asic_info().get("market_name")
+
+def get_gpu_fan_speed():
+    PLATRFORM = detect_platform(0)
+    if PLATRFORM == "Unknown":
+        return 0;
+    elif PLATRFORM == "Nvidia":
+        info = pynvml.nvmlDeviceGetFanSpeedRPM(pynvml.nvmlDeviceGetHandleByIndex(0))
+        return info
+    else:
+        # AMD
+        devices = amdsmi_get_processor_handles()
+        info = amdsmi.amdsmi_get_gpu_fan_rpms(devices[0], 0)
+        return info
+
+def get_temp_vram():
+    PLATRFORM = detect_platform(0)
+    if PLATRFORM == "Unknown":
+        return "0 °C"
+    elif PLATRFORM == "Nvidia":
+        handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+        field = pynvml.c_nvmlFieldValue_t()
+        field.fieldId = pynvml.NVML_FI_DEV_MEMORY_TEMP
+        pynvml.nvmlDeviceGetFieldValues(handle, 1, [field])
+        if field.nvmlReturn == pynvml.NVML_SUCCESS:
+            return str(field_value.value.uiVal) + " °C"
+        else:
+            return "Unsupported"
+    else:
+        # AMD
+        devices = amdsmi_get_processor_handles()
+        temp = amdsmi.amdsmi_get_temp_metric(devices[0], amdsmi.AmdSmiTemperatureType.VRAM , amdsmi.AmdSmiTemperatureMetric.CURRENT)/1000
+        return str(round(temp, 1)) + " °C"
+
+
+# def get_max_min_core_speed()
--- src/tuxafterburner.py
+++ src/tuxafterburner.py
@@ -3,13 +3,31 @@ import customtkinter as ctk
 import platform
 import pyautogui
 import api
+from PIL import Image
+import sys
+import os
 
-os = platform.system()
+os_ = platform.system()
 
 
-if os != "Linux":
+if os_ != "Linux":
     pyautogui.alert("Tux afterburner is supported only on desktop linux systems", "System error")
     exit(1)
+
+PLATFORM = api.get_gpu_name()
+
+def get_asset_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+PATH = ""
+if(PLATFORM=="Nvidia"):
+    PATH = get_asset_path("imgs/NVIDIA_.png")
+#elif PLATFORM == "AMD":
+else:
+    PATH = get_asset_path("imgs/AMD.png")
+
 
 
 
@@ -19,19 +37,37 @@ app = ctk.CTk()
 
 app.title("Tux Afterburner - " + api.get_gpu_name())
 app.geometry("600x480")
-textbox = ctk.CTkTextbox(app, 600, 100)
+textbox = ctk.CTkTextbox(app, 300, 105)
 textbox.pack(pady=20)
+textbox.place(x=0)
+textbox.place(y=10)
+
+
+my_image = ctk.CTkImage(
+    light_image=Image.open(PATH), 
+    dark_image=Image.open(PATH), 
+    size=(250, 100)
+)
+
 
 def stat():
     textbox.configure(state="normal")
     textbox.delete("0.0", "end")
-    textbox.insert("0.0", "Temperature: " + str(api.get_temp()) + " C"
+    textbox.insert("0.0", "Core temperature: " + str(api.get_temp()) + " °C"
+        + "\nVRAM temperature: " + api.get_temp_vram()
         + "\nVRAM total: " + str(api.get_vram()) 
         + "\nVRAM free: " + str(api.get_vram_free())
+        + "\nFans speed: " + str(api.get_gpu_fan_speed()) + " rpm"
     )
     textbox.configure(state="disabled")
     app.after(1000, stat)
-    
+
+image_label = ctk.CTkLabel(app, image=my_image, text="")
+image_label.place(x=320, y=10) 
+
+image_label.image = my_image 
+
+
 stat()
 
 app.mainloop()
